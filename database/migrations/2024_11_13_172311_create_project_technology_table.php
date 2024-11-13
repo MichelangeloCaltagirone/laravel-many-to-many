50f33298c90e1f3107fffbc7f0f3aa5b7475dd28
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-13 18:34:35 +0100

    Create the migration for project-Technology table.

--- database/migrations/2024_11_13_172311_create_project_technology_table.php
+++ database/migrations/2024_11_13_172311_create_project_technology_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId("project_id")->constrained();
+            $table->foreignId("technology_id")->constrained();
+
+            $table->primary(["project_id", "technology_id"]);
             $table->timestamps();
         });
     }
