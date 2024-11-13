e9769cd905b5f0f42cbc8785d8bfcca45f250003
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-13 18:16:29 +0100

    Created Model, Migration, Seeder for Tecnology resource before cloning the repo. Now seeded the db table for Technologies.

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,22 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologyNames = [
+            "Html",
+            "Css",
+            "Java Script",
+            "Php",
+            "Framework Laravel",
+            "Framework Vite",
+            "Front-end",
+            "Back-end",
+            "Server side"
+        ];
+
+        foreach($technologyNames as $name) {
+            $newtechnology = new Technology();
+            $newtechnology->name = $name;
+            $newtechnology->save();
+        };
     }
 }
